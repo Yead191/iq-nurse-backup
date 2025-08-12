160d4d826651836734375538043add3a62b73ce5
Yead191 <yead191@gmail.com>
2025-08-12 17:43:05 +0600

    surgical details on progress

--- src/app/(user-dashboard)/profile/study-notes/document/[id]/page.tsx
+++ src/app/(user-dashboard)/profile/study-notes/document/[id]/page.tsx
@@ -1,11 +1,5 @@
 import SurgicalDetailsPage from "@/components/ui/user-dashboard-pages/study-notes-page/surgical-details-page/SurgicalDetailsPage";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
   return <SurgicalDetailsPage id={params.id} />;
 }
