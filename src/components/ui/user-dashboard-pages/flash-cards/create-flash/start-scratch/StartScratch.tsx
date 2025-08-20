4fb1d4ae57b7d36e87d89a334ecf7615d01b5f41
MithilaKhan <mithilakhan082@gmail.com>
2025-08-20 14:38:30 +0600

    add flash

--- src/components/ui/user-dashboard-pages/flash-cards/create-flash/start-scratch/StartScratch.tsx
+++ src/components/ui/user-dashboard-pages/flash-cards/create-flash/start-scratch/StartScratch.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import {Typography } from 'antd';
+import { Typography } from 'antd';
 import StartScratchCard from './StartScratchCard';
 import { startScratchData } from '@/data/flashCards';
 import { MdOutlineAdd, MdOutlineArrowForward } from 'react-icons/md';
@@ -9,8 +9,8 @@ import { useRouter } from 'next/navigation';
 
 const { Title } = Typography;
 
-const StartScratch = () => { 
-    const [open , setOpen]= useState(false) 
+const StartScratch = () => {
+    const [open, setOpen] = useState(false)
     const router = useRouter()
 
     return (
@@ -21,17 +21,18 @@ const StartScratch = () => {
                     <Title level={2} className='w-full'>Flashcard23601</Title>
                     <div className=" flex lg:flex-row flex-col items-center gap-2 w-full justify-end">
                         <button className=' h-[45px] lg:w-[200px] w-full rounded-lg bg-[#003877] text-white cursor-pointer'>
-                          <span> Study Now </span> 
+                            <span> Study Now </span>
                         </button>
 
-                        <button className='h-[45px] lg:w-[200px] w-full rounded-lg border border-[#003877] text-[#003877] flex items-center justify-center gap-1 cursor-pointer' onClick={()=>setOpen(true)} >
+                        <button className='h-[45px] lg:w-[200px] w-full rounded-lg border border-[#003877] text-[#003877] flex items-center justify-center gap-1 cursor-pointer' onClick={() => setOpen(true)} >
                             <span> <MdOutlineAdd size={20} /> </span>
                             <span> Add Flash </span>
-                        </button> 
+                        </button>
 
-                            <button className=' h-[45px] lg:w-[200px] w-full rounded-lg bg-[#003877] text-white flex items-center justify-center gap-1 cursor-pointer'
-                             onClick={()=>router.push("/profile/flash-cards/create-flash/start-scratch/flash-folder")}>
-                          <span> Continue </span>  <span> <MdOutlineArrowForward size={20} /> </span>
+                        {/* sf */}
+                        <button className=' h-[45px] lg:w-[200px] w-full rounded-lg bg-[#003877] text-white flex items-center justify-center gap-1 cursor-pointer'
+                            onClick={() => router.push("/profile/flash-cards/create-flash/start-scratch/flash-folder")}>
+                            <span> Continue </span>  <span> <MdOutlineArrowForward size={20} /> </span>
                         </button>
 
                     </div>
@@ -49,7 +50,7 @@ const StartScratch = () => {
 
                 </div>
 
-            </div> 
+            </div>
             <AddFlashModal open={open} setOpen={setOpen} />
         </div>
     );
