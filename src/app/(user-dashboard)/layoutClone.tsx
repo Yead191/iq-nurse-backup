188e6f053a26266844f3ab361128b7590c13b738
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-09-20 16:33:34 +0600

    updated

--- src/app/(user-dashboard)/layoutClone.tsx
+++ src/app/(user-dashboard)/layoutClone.tsx
@@ -33,7 +33,8 @@ const LayoutClone = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="bg-[#FFFFFF] ">
-      {/* mobile header */}
+
+      {/* mobile header..*/}
       <div
         className={`   sticky top-0 z-10 ${
           shouldHide || pathname.startsWith("/profile/calendar") || singleHide
